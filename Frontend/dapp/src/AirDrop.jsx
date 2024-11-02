33750f29b7aea3800ecf1996fb4ee295ad67c3d5
SuperexMack <unilexerox@gmail.com>
2024-11-02 21:47:22 +0530

    Added some cool features

--- Frontend/dapp/src/AirDrop.jsx
+++ Frontend/dapp/src/AirDrop.jsx
@@ -1,16 +1,37 @@
 import { useConnection , useWallet } from "@solana/wallet-adapter-react"
-import { useState } from "react"
+import { useEffect, useState , useCallback} from "react"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
 export function AirDrop(){
-   
+
+  
    const [getValue , setValue] = useState(0)
+   const [balance,setBalance] = useState(0)
    let wallet = useWallet()
    let {connection} = useConnection()
 
+   const getTheBalanceData = useCallback(async()=>{
+    if(wallet.publicKey){
+        try{
+            let storeTheBalance = await connection.getBalance(wallet.publicKey)
+            setBalance(storeTheBalance/1e9)
+        }
+        catch(err){
+            console.log("some error : " + err)
+            toast.error("some error : " + err)
+        }
+    }
+    
+   },[wallet.publicKey,connection])
+
+   useEffect(()=>{
+    getTheBalanceData()
+   },[connection,wallet.publicKey])
+
    const sendDrop = async()=>{
+    
     await connection.requestAirdrop(wallet.publicKey , parseInt(getValue))
     setValue("")
     toast.success("Sol Added successfully")
@@ -20,8 +41,8 @@ export function AirDrop(){
     return(
         <>
 
-        <h1 className="relative top-[20px] font-bold text-[30px]">Welcome Sir with Public Key - <span className="text-[20px]">{wallet.publicKey ? wallet.publicKey.toString() : "Guest Account"}</span></h1>              
-   
+        <h1 className="relative top-[20px] font-normal text-[30px]">Welcome Sir with Public Key - <span className="text-[20px]">{wallet.publicKey ? wallet.publicKey.toString() : "Guest Account"}</span></h1> 
+        <p className="text-[20px] font-normal relative top-[20px]">Your Current Balance is  : <span>{balance}</span></p>             
         <div className="flex space-x-9 relative top-[50px]">
         <input value={getValue} onChange={(e)=>setValue(e.target.value)} placeholder="Enter the Amount" className="border-black rounded-lg p-4 font-bold"></input>
         <div className="flex justify-center items-center">
--- Frontend/dapp/src/App.jsx
+++ Frontend/dapp/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     <>
       <div className='h-[1000px] main-screen w-full flex justify-center'>
          <div className='w-[60rem] h-auto flex flex-col items-center space-y-4 p-5 z-50'>
-            <h1 className='mt-20 text-[50px] font-bold text-violet-700'>Welcome to Mack's Wallet Adapter</h1>
+            <h1 className='text-[50px] font-bold text-violet-700'>Welcome to Mack's Wallet Adapter</h1>
             <p className='text-[35px] text-pink-500 font-bold'>This wallet Adapter do not add <img className='inline h-[50px] w-[50px]' src={solana}></img> in real </p>
             <p className='text-[30px] font-medium'>Please Switch to devnet to see these <img className='inline h-[50px] w-[50px]' src={solana}></img></p>
             <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/1GcAeTdxUpNiEvq2IeIo_Cqm97P_J-R4"}>
